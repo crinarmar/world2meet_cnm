1a575341aa289122a57ee9a6578a45e01537e7de
crinarmar <cnaranjo92@gmail.com>
2024-02-25 13:37:53 +0100

    develop

--- W2M_Microservice/domain/src/main/java/com/dekra/service/Product.java
+++ W2M_Microservice/domain/src/main/java/com/dekra/service/Product.java
@@ -20,4 +20,64 @@ public class Product {
 		this.totalPrice = this.price + ((tax / 100.0) * this.price);
 	}
 
+	public static Builder maker() {
+		return new Builder();
+	}
+
+	public static class Builder {
+		private final Product product;
+
+		private Builder() {
+			this.product = new Product();
+		}
+
+		public Builder id(Long id) {
+			product.id = id;
+			return this;
+		}
+
+		public Builder name(String name) {
+			product.name = name;
+			return this;
+		}
+
+		public Builder description(String description) {
+			product.description = description;
+			return this;
+		}
+
+		public Builder price(double price) {
+			product.price = price;
+			return this;
+		}
+
+		public Product build() {
+			return product;
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public Double getTax() {
+		return tax;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
 }
--- W2M_Microservice/jpa/src/main/java/com/dekra/service/ProductEntity.java
+++ W2M_Microservice/jpa/src/main/java/com/dekra/service/ProductEntity.java
@@ -27,4 +27,36 @@ public class ProductEntity {
 	@Column(name = "PRICE", nullable = false, unique = false)
 	private Double price;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 }
